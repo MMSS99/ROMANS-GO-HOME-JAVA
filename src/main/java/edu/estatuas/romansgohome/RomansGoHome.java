838a049e7d8dff924049f56fa0998a889687eb24
M. Marcelo S. Salgado <mmssalgado09@gmail.com>
2025-05-15 12:30:43 +0200

    build(ALL):  :tada: first commit

---
+++ src/main/java/edu/estatuas/romansgohome/RomansGoHome.java
@@ -0,0 +1,9 @@
+package edu.estatuas.romansgohome;
+
+public class RomansGoHome {
+    public static void main(String[] args) {
+
+        // 1. Dividir string con REGEX - 2. Pasarlo por tipos enumerados para calcular
+
+    }
+}
